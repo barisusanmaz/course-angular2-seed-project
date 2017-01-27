18a74603a0c8a148c222894ded23c19d9edef8c3
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-27 10:24:50 +0300

    first commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.config.common.js');
 
 module.exports = webpackMerge(commonConfig,  {
-    devTool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
 
     output: {
         path: '/dist',
