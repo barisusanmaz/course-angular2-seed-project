6838c608b55febeb5738af283abad1881683e1cf
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-27 13:45:53 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home.component";
+import {routing} from "./app.routing";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        routing
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
